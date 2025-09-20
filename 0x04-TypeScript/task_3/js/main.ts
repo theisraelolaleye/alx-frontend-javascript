7eba28d0cef0d901a6165705c461d70fdd1aace6
theisraelolaleye <theisraelolaleye@gmail.com>
2025-09-20 19:57:09 +0100

    8. Ambient Namespaces

--- 0x04-TypeScript/task_3/js/main.ts
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -10,9 +10,7 @@ const row: RowElement = {
 
 const newRowID: RowID = CRUD.insertRow(row);
 
-const updatedRow: RowElement = {
-  ...row, age: 23,
-};
+const updatedRow: RowElement = { ...row, age: 23, };
 
 CRUD.updateRow(newRowID, updatedRow);
 CRUD.deleteRow(newRowID);
